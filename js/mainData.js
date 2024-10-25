0f825e93293c3a86d48ad39114bd03d3934c7762
Karina <karinakirillova897@gmail.com>
2024-10-25 19:30:10 +0300

    new

--- js/mainData.js
+++ js/mainData.js
@@ -1,9 +1,41 @@
 const mainData = () => {
+  const renderAnimeList = (arr, ganres) => {};
+  const renderTopAnime = (arr) => {
+    const wripper = document.querySelector(".filter__gallery");
+    wripper.innerHTML = ``;
+
+    arr.forEach((el) => {
+      wripper.insertAdjacentHTML(
+        "afterbegin",
+        `<div class="product__sidebar__view__item set-bg mix day years"
+            data-setbg="${el.image}">
+            <div class="ep">${el.rating} / 10</div>
+            <div class="view"><i class="fa fa-eye"></i> ${el.views}</div>
+            <h5><a href="/anime-details.html">${el.title}</a></h5>
+        </div>`
+      );
+    });
+
+    wripper.querySelectorAll(".set-bg").forEach((el) => {
+      el.style.backgroundImage = `url(${el.dataset.setbg})`;
+    });
+  };
+
   fetch("./db.json")
     .then((response) => {
       return response.json();
     })
-    .then((data) => {});
+    .then((data) => {
+      const ganres = new Set();
+
+      renderTopAnime(data.anime.sort((a, b) => b.views - a.views).slice(0, 5));
+
+      data.anime.forEach((el) => {
+        ganres.add(el.ganre);
+      });
+
+      renderAnimeList(data.anime, ganres);
+    });
 };
 
 mainData();
